2405766da7bfce986f16836715183af4212ae451
Lnrz <loreevil@gmail.com>
2024-12-15 21:58:33 +0100

    Change game.py according to the behaviour of settings

--- src/game.py
+++ src/game.py
@@ -3,9 +3,7 @@ from grid_agent.settings import GameSettings, CommandLineSettings
 from grid_agent.data_structs import Result
 
 def main():
-  cmd_line_set: CommandLineSettings = CommandLineSettings()
-  cmd_line_set.parse()
-  game_set : GameSettings = GameSettings(cmd_line_set)
+  game_set : GameSettings = GameSettings(CommandLineSettings())
   game_manager: GameManager = GameManager(game_set)
   res: Result = game_manager.start()
   print(res.name)
